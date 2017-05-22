41b529a7c857b541c5280a9c8e9f82d7a035db75
Mitchell Sotto <sottochoro@gmail.com>
2017-05-22 11:29:24 -0600

    got force function to execute without needing a parameter

--- library.js
+++ library.js
@@ -2,120 +2,140 @@
 //-----------------------------------------
    
 (function(window) {
-   function defineVader(){
-      var Vader = function(search_term){
-      	var target = '';
-      	switch(search_term.substr(0,1)) {
-	      			case '#':
-	      				target = document.getElementById(search_term.substr(1));
-      	}
+    function defineForce(){
+      
+	      var Force = function(search_term){
+	      	var target = '';
 
-//define the library
-      	var Library = function(element) {
-	      	console.log(element);
-	      	this.elem = element;
-	      };
+	      	if(search_term === undefined) {
+	      			
 
-	     var element = Library.prototype;
 
-	   element.color = function(color) {
-		  this.elem.style.color = color;
-		  return this;
-		};
+      		//define the library
+	      	var Other = function() {
+		      	this.body = "No Component";
+		      };
 
-		element.size = function(height, width) {
-			this.elem.style.height = height;
-			this.elem.style.width = width;
-			return this;
-		};
+		     var element = Other.prototype;
 
-		element.border = function(one, two, three) {
-			this.elem.style.border = one + " " + two + " " + three;
-			return this;
-		};
+		     element.feelings = function(feelings) {
+				console.log("feelings", feelings);
+				alert(feelings);
+				return this;
+			};
+				
+			element.makeComponent = function(componenetName, template) {
+				class NewComponent extends HTMLElement {
+					static get observedAttributes() {
+			            return ['disabled', 'open'];
+			          }
+			        
+			        constructor() {
+						super();
+						let shadowRoot = this.attachShadow({mode: 'open'});
+						shadowRoot.innerHTML = template;
+					}
+			        
+					get value() {
+						return this.shadowRoot.querySelector('input').value;     
+					}
 
-		element.forcePushRight = function(distance) {
-			this.elem.style.marginLeft = distance;
-		};
-
-		element.feelings = function(feelings) {
-			alert(feelings);
-		};
-
-		element.click = function(){
-			
-		};
-
-		element.makeComponent = function(template) {
-			
-			
-
-			class SearchComponent extends HTMLElement {
-				static get observedAttributes() {
-		            return ['disabled', 'open'];
-		          }
-		        
-		        constructor() {
-					super();
-					let shadowRoot = this.attachShadow({mode: 'open'});
-					shadowRoot.innerHTML = template;
-		            
-		           // const input = this.shadowRoot.querySelector('input'); input.addEventListener('keypress', function(e){
-		           //      if(e.which === 13) {
-		           //          console.log(input.value);
-		           //      }
-		           //  });
-		            
-		           //  const button = this.shadowRoot.querySelector('button');
-		           //  button.addEventListener('click', function(e){
-		           //      console.log(input.value);
-		           //  })
+					set value(value) {
+			            this.setAttribute("value", value);
+			            return this.shadowRoot.querySelector('input').value = value;
+					}
 				}
-		        
-				get value() {
-					return this.shadowRoot.querySelector('input').value;     
-				}
+				this.body = window.customElements.define(componenetName, NewComponent);
+				return this;
+			};
+			// end define the Other--------------
 
-				set value(value) {
-		            this.setAttribute("value", value);
-		            return this.shadowRoot.querySelector('input').value = value;
-				}
-			}
 
-			return window.customElements.define('my-component', SearchComponent);
+
+			var Other = new Other();
+			
+			return Other;		   
+
+
+
+
+
+
+
+			 } else {
+
+
+
+
+ 				switch(search_term.substr(0,1)) {
+      				case '#':
+      				target = document.getElementById(search_term.substr(1));
+		      	}
+
+		//define the library
+		      	var Library = function(element) {
+			      	// console.log(element);
+			      	this.elem = element;
+			      };
+
+			     var element = Library.prototype;
+
+			   element.color = function(color) {
+				  this.elem.style.color = color;
+				  return this;
+				};
+
+				element.size = function(height, width) {
+					this.elem.style.height = height;
+					this.elem.style.width = width;
+					return this;
+				};
+
+				element.border = function(one, two, three) {
+					this.elem.style.border = one + " " + two + " " + three;
+					return this;
+				};
+
+				element.forcePushRight = function(distance) {
+					this.elem.style.marginLeft = distance;
+				};
+
+
+			
+
+
+			//Japanese function calls...and they Work!!!
+				element.色 = function(color) {
+				this.elem.style.color = color;
+				  return this;
+				};
+
+				element.いろ = function(color) {
+				this.elem.style.color = color;
+				  return this;
+				};
+		// end define the library--------------
+
+
+
+				var library = new Library(target);
+				return library;
+
+
+
+
+			 }
+
 		};
+		return Force;
+	};
 
-
-
-
-
-
-
-	//Japanese function calls...and they Work!!!
-		element.色 = function(color) {
-		this.elem.style.color = color;
-		  return this;
-		};
-
-		element.いろ = function(color) {
-		this.elem.style.color = color;
-		  return this;
-		};
-// end define the library--------------
-
-
-
-		var library = new Library(target);
-		return library;
-      };
-
-      return Vader;
-   };
+	      	
    
-   if(typeof Vader === 'undefined'){
-      window.Vader = defineVader();
+   if(typeof Force === 'undefined'){
+      window.Force = defineForce();
    }
-   
+
 })(window);
 
 
@@ -128,53 +148,4 @@
 
 
 
-
-
-
-(function() {
-	const template = `
-		<style>
-            
-		</style>
-		<div>
-            <input class="blue" type="text" value="" name="search">
-			
-            <button>Submit</button>
-		</div>		
-	`;
-
-	class SearchComponent extends HTMLElement {
-		static get observedAttributes() {
-            return ['disabled', 'open'];
-          }
-        
-        constructor() {
-			super();
-			let shadowRoot = this.attachShadow({mode: 'open'});
-			shadowRoot.innerHTML = template;
-            
-           // const input = this.shadowRoot.querySelector('input'); input.addEventListener('keypress', function(e){
-           //      if(e.which === 13) {
-           //          console.log(input.value);
-           //      }
-           //  });
-            
-           //  const button = this.shadowRoot.querySelector('button');
-           //  button.addEventListener('click', function(e){
-           //      console.log(input.value);
-           //  })
-		}
-        
-		get value() {
-			return this.shadowRoot.querySelector('input').value;     
-		}
-
-		set value(value) {
-            this.setAttribute("value", value);
-            return this.shadowRoot.querySelector('input').value = value;
-		}
-	}
-
-	window.customElements.define('search-input', SearchComponent);
-})();
 
